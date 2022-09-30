cf1f59337f56183c0039e96f59c1d5c06e0eb648
ReflextionsDev <reflextionsdev@gmail.com>
2022-09-30 18:42:35 -0400

    mui imagelist

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 export default function Photo(props) {
 
-    const {src, alt} = props
+    const { src, alt } = props
 
-  return (
-    <div>
-      <img src={src} alt={alt}></img>
-    </div>
-  )
+    return (
+
+        <img
+            src={`${src}?w=248&fit=crop&auto=format`}
+            srcSet={`${src}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={alt}
+            loading="lazy"
+        />
+
+    )
 }
--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -3,9 +3,19 @@
 import React from 'react'
 import Photo from './Photo'
 
+import Button from '@mui/material/Button';
+import { ImageList, Container, Link } from '@mui/material';
+
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+
+// import IconButton from '@mui/material/IconButton';
+// import InfoIcon from '@mui/icons-material/Info';
 
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
+
 
 
 export default function Photos(props) {
@@ -20,23 +30,69 @@ export default function Photos(props) {
 
 
         <div>
-            {
-                (
-                    photos && photos.map((photo, idx) =>
-                        <div
-                            key={idx}
-                        >
-                            {/* {photo.src.small} */}
-                            {/* componetize this into a photo */}
-                            {/* <img src={photo.src.small}></img> */}
-                            <Photo src={photo.src.medium} alt={photo.alt} />
-                        </div>
+
+            <Button variant="contained">Hello World</Button>
+
+            <Container>
+
+
+                {
+                    (
+                        photos
+                        &&
+
+                        <ImageList cols={5} >
+
+                            <ImageListItem key="Subheader" cols={5}>
+                                <ListSubheader component="div">December</ListSubheader>
+                            </ImageListItem>
+
+                            {photos.map((photo, idx) =>
+
+                                <ImageListItem key={idx}>
+                                    {/* {photo.src.small} */}
+                                    {/* componetize this into a photo */}
+                                    {/* <img src={photo.src.small}></img> */}
+                                    {/* <Photo src={photo.src.small} alt={photo.alt} /> */}
+
+                                    <img
+                                        src={`${photo.src.small}?w=248&fit=crop&auto=format`}
+                                        srcSet={`${photo.src.small}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                        alt={photo.alt}
+                                        loading="lazy"
+                                    />
+
+                                    <ImageListItemBar
+
+                                        title={`${photo.photographer}`}
+                                        subtitle=<Link href={photo.photographer_url} color="inherit">
+                                            {photo.photographer_url.split('@')[1]}
+
+                                        </Link>
+
+                                    // actionIcon={
+                                    //     <IconButton
+                                    //         sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                    //         aria-label={`info about $`}
+                                    //     >
+                                    //         <InfoIcon />
+                                    //     </IconButton>
+                                    // }
+                                    />
+
+                                </ImageListItem>
+                            )
+                            }
+                        </ImageList>
+
+
                     )
-                )
 
-                || <Skeleton />
-            }
+                    || <Skeleton />
+                }
 
+
+            </Container>
 
 
         </div>
