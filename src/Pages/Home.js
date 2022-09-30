7f36b9423342373ba4b93457d170d38282be004c
ReflextionsDev <reflextionsdev@gmail.com>
2022-09-30 16:15:34 -0400

    scaffolding

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+// Need cookies to store session > page number, and search query, if no query default to curated
+// Test case, load curated on startup
+// Implemented loading skeleton
+// Implement MUI
+// Scaffold layout
+// Add features
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
