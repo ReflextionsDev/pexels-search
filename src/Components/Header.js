7fc599e53267f5fa6eaeaf96463dec8425ed587c
ReflextionsDev <reflextionsdev@gmail.com>
2022-10-01 14:15:16 -0400

    curated pagination

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,25 +3,15 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
-
-
-            <Box display="flex" justifyContent="center" sx={{ p: 2, bgcolor: '#e0edff', boxShadow: 3,  }} >
-                <Stack
-                    direction={{ xs: 'column', sm: 'row' }}
-                    spacing={{ xs: 1, sm: 2, md: 4 }}
-                >
-
-                    <TextField id="outlined-basic" label="Search" variant="outlined" />
-                    <Button variant="contained">Search</Button>
-                    <Button variant="contained">Reset</Button>
-
-                </Stack>
-
-            </Box>
-
-
-
-        </div>
+        <Box display="flex" justifyContent="center" sx={{ p: 2, bgcolor: '#e0edff', boxShadow: 3, }} >
+            <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 1, sm: 2, md: 4 }}
+            >
+                <TextField id="outlined-basic" label="Search" variant="outlined" />
+                <Button variant="contained">Search</Button>
+                <Button variant="contained">Reset</Button>
+            </Stack>
+        </Box>
     )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,38 +11,67 @@ import Footer from '../Components/Footer'
 import Header from '../Components/Header'
 import Photos from '../Components/Photos'
 
+const domain = 'http://localhost:3000'
+
 
 // Will need to adjust later based on cache
 // Fetch curated on load
 
 export default function Home() {
 
+
+
+    const [page, setPage] = useState(1)
+    const [pageCount, setPageCount] = useState(10)
+
+
+    const changePage = (event, value) => {
+        console.log(value)
+        setPage(value)
+        console.log(page)
+    }
+
+
     const [photos, setPhotos] = useState()
 
     const fetchCurated = async () => {
         try {
-            const response = await fetch('http://localhost:3000/pexels/curated/1', {})
+            const response = await fetch(`${domain}/pexels/curated/${page}`, {})
             let JSON = await response.json()
             setPhotos(JSON.payload.photos)
 
-            // console.log(JSON.payload.photos)
+            console.log(JSON.payload)
             // console.log('photos:', photos)
+
+            updatePageCount(JSON.payload.total_results, JSON.payload.per_page)
+
         } catch (error) {
             console.log('fetch error', error)
         }
     }
 
 
+    const updatePageCount = (total, perPage) => {
+        console.log(total, perPage)
+        const totalPages = Math.ceil(total / perPage)
+        setPageCount(totalPages)
+    }
+
+
+
     useEffect(() => {
         fetchCurated()
-    }, [])
+    }, [page])
 
 
     return (
         <div>
             <Header />
             <Photos photos={photos} />
-            <Footer />
+            {/* <Footer /> */}
+            <Box display="flex" justifyContent="center">
+                <Pagination sx={{ p: 2 }} count={pageCount} color="primary" onChange={changePage} />
+            </Box>
         </div>
     )
 }
